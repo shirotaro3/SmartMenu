42524a876484c82095e601c42a49c7fbd2224b38
Yusuke Shirota <winterleaves2219.443@gmail.com>
2019-09-24 20:25:26 +0900

    [fix]migrate files

--- db/migrate/20190903054847_create_requests.rb
+++ db/migrate/20190903054847_create_requests.rb
@@ -2,7 +2,7 @@ class CreateRequests < ActiveRecord::Migration[5.2]
   def change
     create_table :requests do |t|
       t.integer :shop_id, null: false
-      t.text :message, null: false, default: ''
+      t.text :message, null: false
       t.string :title, null: false, default: ''
       t.timestamps
     end
