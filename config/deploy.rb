4c6db44ed1b3902dda8e9c67614e5eb8e50864fa
Yusuke Shirota <winterleaves2219.443@gmail.com>
2019-09-25 03:30:26 +0900

    fix for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -78,5 +78,16 @@ namespace :deploy do
       end
     end
   
+    desc 'db_seed'
+    task :db_seed do
+      on roles(:db) do |host|
+        with rails_env: fetch(:rails_env) do
+          within current_path do
+            execute :bundle, :exec, :rake, 'db:seed'
+          end
+        end
+      end
+    end
+
     before :starting, :confirm
   end
