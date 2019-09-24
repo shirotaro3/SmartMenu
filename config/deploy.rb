d32e4887ac4b7df1008c9795541092ba478d0b90
Yusuke Shirota <winterleaves2219.443@gmail.com>
2019-09-24 21:19:29 +0900

    [fix]gemfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -48,8 +48,14 @@ set :rbenv_ruby, '2.5.5'
 set :log_level, :debug
 
 namespace :deploy do
-    install_plugin Capistrano::Unicorn
   
+    desc "Restart Application"
+    task :restart do
+      on roles(:app), in: :sequence, wait: 5 do
+        invoke 'puma:restart'
+      end
+    end
+
     desc 'Create database'
     task :db_create do
       on roles(:db) do |host|
